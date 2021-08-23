8c86dd7cfd7a109f68c66019b264b4312a298ca5
Reo Yamanaka <ryamanaka807@gmail.com>
2021-08-23 17:23:20 -0400

    revamped script to measure time efficiency

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import memory_profiler
+import time
+
+processPerformance = {"processA": 0}
+
+sampleData = {}
+for i in range(10000000):
+    sampleData[i] = i * i
+
+startTime = time.time()
+
+for i in sampleData.keys():
+    if sampleData[i] == 1000000 * 1000000:
+        print("%d found!" % (1000000 * 1000000))
+        break
+processPerformance["processA"] = time.time() - startTime
+
+print("Finding the element took %f seconds." % processPerformance["processA"])
+
