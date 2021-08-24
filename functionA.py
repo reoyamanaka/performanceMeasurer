d95c6408450457e2cc12ebfdadce463c59eca3e7
Reo Yamanaka <ryamanaka807@gmail.com>
2021-08-24 17:45:34 -0400

    created separate function for more organized performance testing

---
+++ functionA.py
@@ -0,0 +1,10 @@
+def functionA():
+    sampleData = {}
+    for i in range(10000000):
+        sampleData[i] = i * i
+    for i in sampleData.keys():
+        if sampleData[i] == 1000000 * 1000000:
+            print("%d found!" % (1000000 * 1000000))
+            break
+    print("FunctionA has completed.")
+    
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import time
+from functionA import functionA
 
-processPerformance = {"processA": 0}
-
-sampleData = {}
-for i in range(10000000):
-    sampleData[i] = i * i
-
+processPerformance = {"functionA": 0}
 startTime = time.time()
 
-for i in sampleData.keys():
-    if sampleData[i] == 1000000 * 1000000:
-        print("%d found!" % (1000000 * 1000000))
-        break
-processPerformance["processA"] = time.time() - startTime
+functionA()
 
-print("Finding the element took %f seconds." % processPerformance["processA"])
+processPerformance["functionA"] = time.time() - startTime
+
+print("Finding the element took %f seconds." % processPerformance["functionA"])
 
